3b317e5cdddc6ee618e301f5ab6958544c05e83a
Yureka <yuka@yuka.dev>
2021-07-06 21:17:51 +0200

    fix firewall for nfsv3

--- services/nfs-server/default.nix
+++ services/nfs-server/default.nix
@@ -1,9 +1,17 @@
 { ... }:
 
 {
-  services.nfs.server.enable = true;
-  services.nfs.server.exports = ''
-    /mnt 172.23.42.0/24(rw,fsid=0,insecure,no_subtree_check) fd00::/8(rw,fsid=0,insecure,no_subtree_check)
-  '';
-  networking.firewall.allowedTCPPorts = [ 2049 ];
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /mnt 172.23.42.0/24(rw,fsid=0,insecure,no_subtree_check) fd00::/8(rw,fsid=0,insecure,no_subtree_check)
+    '';
+
+    statdPort = 4000;
+    lockdPort = 4001;
+    mountdPort = 4002;
+  };
+
+  networking.firewall.interfaces.enp3s0.allowedTCPPorts = [ 2049 4000 4001 4002 111 ];
+  networking.firewall.interfaces.enp3s0.allowedUDPPorts = [ 2049 4000 4001 4002 111 ];
 }
