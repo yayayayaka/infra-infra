d0e3773d1362e0f0a2ccef6230fe6ca2634457e5
Yureka <yuka@yuka.dev>
2021-11-25 02:45:16 +0100

    add presence-web

--- services/presence-monitor/default.nix
+++ services/presence-monitor/default.nix
@@ -34,6 +34,39 @@ let
     [global.databases]
     sqlite_presence_monitor = { url = "/var/lib/presence-monitor/db.sqlite" }
   '';
+
+  npmlock2nix = pkgs.callPackage <npmlock2nix> {};
+
+  presence-web = pkgs.callPackage (
+    { stdenv }:
+
+    stdenv.mkDerivation rec {
+      pname = "presence-web";
+      version = "0.0.0";
+
+      src = builtins.fetchGit {
+        url = "https://gitlab.com/luxferresum/presence-web/";
+        rev = "bbddb356bd38c136d5adee49cebb1b95ae7b2ef4";
+      };
+
+      node_modules = npmlock2nix.node_modules { inherit src; };
+
+      buildPhase = ''
+        runHook preBuild
+        export HOME=$(mktemp -d)
+        ln -s $node_modules/node_modules node_modules
+        node_modules/.bin/ember build --environment production
+        runHook postBuild
+      '';
+
+      installPhase = ''
+        runHook preInstall
+        cp -r dist $out
+        runHook postInstall
+      '';
+    }
+  ) {};
+
 in {
   secrets.presence-monitor-env = {};
 
@@ -54,6 +87,10 @@ in {
     virtualHosts.default = {
       locations."/presence/api/" = {
         proxyPass = "http://localhost:8000/";
+      };
+      locations."/presence/" = {
+        alias = "${presence-web}/";
+        tryFiles = "$uri $uri/ /presence/index.html";
       };
     };
   };
