3eb679ef76b854c87dc64871077d1a53a4ef23f4
Yureka <yuka@yuka.dev>
2024-01-19 13:50:51 +0100

    add new key for multi

--- users/multi/default.nix
+++ users/multi/default.nix
@@ -6,6 +6,7 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWtfUhmD0hgr2u4EOfFUWsrtfxl9IZ9LGjqLjL/QzeN multi@flywheel"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDhio/0DSCdbAttE+v+exbjlmXTzVtS57ehU5JO4kIHL8MWM+7RLhodAtY5r4ZHWMtjWJteYnNlt+m3FGRjnj6A= multi@macbook"
     ];
     shell = pkgs.bash;
   };
